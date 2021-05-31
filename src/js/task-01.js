9f36b32a20b913e450c340c04879084603f8426f
valeraron <77055501+valeraron@users.noreply.github.com>
2021-05-31 18:46:45 +0300

    скрипт на поиск Allitem

---
+++ src/js/task-01.js
@@ -0,0 +1,7 @@
+const Allitem = function (li) {
+  const item = document.querySelectorAll('.item');
+  const lengthItem = item.length;
+
+  return console.log(`У списку ${lengthItem} категорії.`);
+};
+Allitem();
