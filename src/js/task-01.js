011d78faf9465481f05c62457f0f31d28adec3e9
valeraron <77055501+valeraron@users.noreply.github.com>
2021-06-02 19:10:57 +0300

    первое задание пробы

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,7 +1,40 @@
-const Allitem = function (li) {
+// Напиши скрипт, який виконає наступні операції.
+
+// Порахує і виведе в консоль кількість категорій в `ul#categories`, тобто
+// елементів `li.item`. Вийде `'У списку 3 категорії.'`.
+
+// Для кожного елемента `li.item` в списку `ul#categories`, знайде і виведе в
+// консоль текст заголовка елемента (тега h2) і кількість елементів в категорії
+// (всіх вкладених в нього елементів `li`).
+
+// Наприклад, для першої категорії вийде:
+
+// - Категорія: Тварини
+// - Кількість елементів: 4
+
+const itemlength = function () {
   const item = document.querySelectorAll('.item');
   const lengthItem = item.length;
-
   return console.log(`У списку ${lengthItem} категорії.`);
 };
-Allitem();
+itemlength();
+const ulCategories = document.querySelector('#categories');
+
+const itemlifirst = ulCategories.firstElementChild;
+
+const itemlilast = ulCategories.lastElementChild;
+
+const elemUl = ulCategories.querySelectorAll('ul');
+const title = ulCategories.querySelectorAll('h2');
+
+console.log(elemUl);
+console.log(title);
+const elemBytitle = title.forEach((elem) => {
+  return console.log(elem.textContent);
+});
+console.log(elemBytitle);
+
+const elemByul = elemUl.forEach((elem) => {
+  return console.log(elem);
+});
+console.log(elemByul);
